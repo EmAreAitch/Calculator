ee2cd5a537851b12f478e8c4972ae6f52dbea6fc
EmAreAitch <rahibhasan95@gmail.com>
2022-11-27 23:28:03 +0530

    Added Calculation with Brackets

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ const allClear = document.querySelector('.allClear');
 const clear = document.querySelector('.clear')
 const equal = document.querySelector('.equal');
 const dot = document.querySelector('.dot');
+const brackets = document.querySelector('.brackets')
+const minus = document.querySelector('.minus')
 
+oBracNo = 0;
+cBracNo = 0;
 // let input = [];
 function calculation(equation) {
     if (!(equation.includes(" ÷ 0"))) {
@@ -18,44 +22,65 @@ function calculation(equation) {
         else return item;
     });
 
-    tier1 = input.filter(item => item == '÷' || item == '×' || item == '%');
-    tier2 = input.filter(item => item == '+' || item == '-');
-
-    tier1.forEach(element => {
-        arg1 = input[input.indexOf(element)-1];
-        arg2 = input[input.indexOf(element)+1] || 1;
-        let ans;
-        if (element == '×') {
-            ans = arg1 * arg2;
-            input.splice(input.indexOf(element)-1, 3, ans);
-        }
-        else if (element == '÷') {
-            ans = arg1 / arg2;
-            input.splice(input.indexOf(element)-1, 3, ans);
-        }
-        else if (element == '%') {
-            ans = arg1 * arg2 / 100;
-            input.splice(input.indexOf(element)-1, 3, ans);
-        }
-    });
-
-    tier2.forEach(element => {
-        arg1 = input[input.indexOf(element)-1];
-        arg2 = input[input.indexOf(element)+1];
-        let ans;
-        if (element == '+') {
-            ans = arg1 + arg2;
-            input.splice(input.indexOf(element)-1, 3, ans);
-        }
-        else if (element == '-') {
-            ans = arg1 - arg2;
-            input.splice(input.indexOf(element)-1, 3, ans);
-        }
-    });
+    bracketsCalc(input)
+    tier1Calc(input);
+    tier2Calc(input);
 
     result.textContent = Math.round(input[0]*100)/100;}
     
     else alert("Don't divide by zero you RETARD!!");
+}
+
+function tier1Calc(equ) {
+    tier1 = equ.filter(item => item == '÷' || item == '×' || item == '%');
+    tier1.forEach(element => {
+        arg1 = equ[equ.indexOf(element)-1];
+        arg2 = equ[equ.indexOf(element)+1] || 1;
+        let ans;
+        if (element == '×') {
+            ans = arg1 * arg2;
+
+            equ.splice(equ.indexOf(element)-1, 3, ans);
+        }
+        else if (element == '÷') {
+            ans = arg1 / arg2;
+            equ.splice(equ.indexOf(element)-1, 3, ans);
+        }
+        else if (element == '%') {
+            ans = arg1 * arg2 / 100;
+            equ.splice(equ.indexOf(element)-1, 3, ans);
+        }
+    });
+}
+
+function tier2Calc(equ) {
+    tier2 = equ.filter(item => item == '+' || item == '-');
+    tier2.forEach(element => {
+        arg1 = equ[equ.indexOf(element)-1];
+        arg2 = equ[equ.indexOf(element)+1];
+        let ans = 0;
+        if (element == '+') {
+            ans = arg1 + arg2;
+            equ.splice(equ.indexOf(element)-1, 3, ans);
+        }
+        else if (element == '-') {
+            ans = arg1 - arg2;
+            equ.splice(equ.indexOf(element)-1, 3, ans);
+        }
+    });
+}
+
+function bracketsCalc(equ) {
+    oBrac = equ.lastIndexOf('(')
+    while (oBrac !== -1) {
+        // oBrac = input.lastIndexOf('(')
+        cBrac = equ.indexOf(')') !== -1 ? equ.indexOf(')') : equ.length
+        bracEqu = equ.slice((oBrac+1),cBrac)
+        tier1Calc(bracEqu)
+        tier2Calc(bracEqu)
+        equ.splice(oBrac, cBrac-oBrac+1 || equ.length , bracEqu[0])
+        oBrac = equ.lastIndexOf('(')
+    }
 }
 
 numbers.forEach(number => {
@@ -77,16 +102,21 @@ operators.forEach(operator => {
 allClear.addEventListener('click', () => {
     _calculation.textContent='';
     result.textContent='';
+    oBracNo = 0;
+    cBracNo = 0;
 })
 
 clear.addEventListener('click', () => {
-    _calculation.textContent.slice(-1) !== ' ' 
+    !isNaN(parseInt(_calculation.textContent.slice(-1)))
     ? _calculation.textContent = _calculation.textContent.slice(0,-1)
-    : _calculation.textContent = _calculation.textContent.slice(0,-3);
+    : _calculation.textContent.slice(-2).includes('( ') || _calculation.textContent.slice(-2).includes(' )')
+        ? _calculation.textContent = _calculation.textContent.slice(0,-2)
+        : _calculation.textContent = _calculation.textContent.slice(0,-3);
 })
 
 equal.addEventListener('click', () => {
-    _calculation.textContent != '' ? calculation(_calculation.textContent) : alert("Error!!");
+    _calculation.textContent != '' && _calculation.textContent.slice(-1) !== ' '
+     ? calculation(_calculation.textContent) : alert("Error!!");
 })
 
 dot.addEventListener('click', (e) => {
@@ -96,3 +126,11 @@ dot.addEventListener('click', (e) => {
             : _calculation.textContent += '.'
         : _calculation.textContent += '0.'
        })
+
+brackets.addEventListener('click', () => {
+     _calculation.textContent !== '' && _calculation.textContent.slice(-1) !== ' '
+    ? oBracNo > cBracNo
+        ? (_calculation.textContent += ' )', cBracNo++)
+        : (_calculation.textContent += ' × ( ', oBracNo++)
+    : (_calculation.textContent += '( ',oBracNo++)
+})
